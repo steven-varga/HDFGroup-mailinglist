48311c692f68bc447c77ffdc33c313b31ab1c020
steven varga <steven@vargaconsulting.ca>
2020-05-06 23:28:55 -0400

    let's don't use h5::flush for now

--- append_move_ctor/generated.h
+++ append_move_ctor/generated.h
@@ -1,8 +1,8 @@
 /* Copyright (c) 2018 vargaconsulting, Toronto,ON Canada
  *     Author: Varga, Steven <steven@vargaconsulting.ca>
  */
-#ifndef H5CPP_GUARD_ZlLSe
-#define H5CPP_GUARD_ZlLSe
+#ifndef H5CPP_GUARD_wRUwW
+#define H5CPP_GUARD_wRUwW
 
 namespace h5{
     //template specialization of sn::log to create HDF5 COMPOUND type
--- append_move_ctor/packettable.cpp
+++ append_move_ctor/packettable.cpp
@@ -1,3 +1,4 @@
+#include <gperftools/profiler.h>
 #include <cstdint>
 #include "struct.h"
 #include <h5cpp/core>
@@ -5,7 +6,8 @@
 #include <h5cpp/io>
 #include <chrono>
 
-int main(){
+
+int main(int argc, char **argv) {
 
 	h5::fd_t fd = h5::create("example.h5",H5F_ACC_TRUNC);
     {
@@ -14,12 +16,13 @@ int main(){
         std::cout << pt;
 
         pt = h5::create<sn::log>(fd, "log.000",
-                h5::max_dims{ H5S_UNLIMITED }, h5::chunk{ 10 }| h5::gzip{9}  );
+                h5::max_dims{ H5S_UNLIMITED }, h5::chunk{ 2 }  );
         std::cout << pt;
-            h5::append(pt, x ); x.idx++; // std::cout << pt;
-            h5::append(pt, x ); x.idx++; //std::cout << pt;
-            h5::append(pt, x ); x.idx++; //std::cout << pt;
+            h5::append(pt, x ); x.idx++;  std::cout << pt;
+            h5::append(pt, x ); x.idx++;  std::cout << pt;
+            h5::append(pt, x ); x.idx++;  std::cout << pt;
         pt = h5::pt_t();
+
         pt = h5::create<sn::log>(fd, "log.001",
                 h5::max_dims{ H5S_UNLIMITED }, h5::chunk{ 10 } | h5::gzip{9} );
             h5::append(pt, x ); x.idx++; //std::cout << pt;
@@ -41,25 +44,24 @@ int main(){
             h5::append(pt, x ); x.idx++; std::cout << pt;
     }
     {
+
         sn::log x; x.idx=0;
         long long size=0;
         h5::pt_t pt;
 		std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-        for (int i=0; i < 100'000; i++){
+     //   ProfilerStart( (std::string(argv[0]) + std::string(".prof")).data() );
+        for (int i=0; i < 10'000; i++){
             pt = h5::create<sn::log>(fd, "log." + std::to_string(i),
                 h5::max_dims{ H5S_UNLIMITED }, h5::chunk{ 1024 }  );
-            for (int j=0; j<1'024; j++) size++, x.idx = j, h5::append(pt, x );
+            for (int j=0; j<10'000; j++) size++, x.idx = j, h5::append(pt, x );
         }
+     //   ProfilerStop();
         std::chrono::system_clock::time_point stop = std::chrono::system_clock::now();
 		double running_time = 1e-6 * std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
 		std::cout << "record size: " << sizeof(sn::log) << "\n";
 		std::cout << "number of mu seconds: " << running_time << " record per sec: " << size/running_time
 	 		<< " sustained throughput: " << ((size/running_time)*sizeof(sn::log )) / 1'000'000 <<" Mbyte/sec"	<< std::endl;
 		std::cout << "transferred data: " << 1e-6 * size * sizeof(sn::log) << "Mbyte\n";
-
-
     }
-
-
 	return 0;
 }
