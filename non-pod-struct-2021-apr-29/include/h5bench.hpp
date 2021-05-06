490ab071d1dc7a7d6fe016df5f4a52aa231732e6
steven varga <steven@vargaconsulting.ca>
2021-05-06 09:28:32 -0400

    added fmt lib, benchmarks, h5::append<std::string>(...)

--- non-pod-struct-2021-apr-29/include/h5bench.hpp
+++ non-pod-struct-2021-apr-29/include/h5bench.hpp
@@ -1,5 +1,9 @@
+
+
+#define FMT_HEADER_ONLY
+
 #include <hdf5.h>
-#include <h5cpp/core>
+#include <h5cpp/all>
 #include <vector>
 #include <string>
 #include <functional>
@@ -10,50 +14,11 @@
 #include <iterator>
 #include <stdint.h>
 #include "meta.hpp"
+#include "struct.hpp"
+#include <fmt/core.h>
+#include <fmt/chrono.h>
+#include <fmt/color.h>
 
-namespace h5::bench {
-    struct test_t {
-        using max_lenght = std::integral_constant<size_t,40>;
-        uint16_t warmup;
-        uint16_t iteration;
-
-        uint64_t min_runtime;
-        uint64_t avg_runtime;
-        uint64_t max_runtime;
-
-        double min_throughput;
-        double avg_throughput;
-        double max_throughput;
-
-        char name[max_lenght::value];
-    };
-}
-
-namespace h5 {
-    //template specialization of shim::pod_t to create HDF5 COMPOUND type
-    template<> hid_t inline register_struct<h5::bench::test_t>(){
-        hid_t name_t = H5Tcopy(H5T_C_S1);
-        H5Tset_size(name_t, H5T_VARIABLE);
-
-        hid_t ct_00 = H5Tcreate(H5T_COMPOUND, sizeof (shim::pod_t));
-        H5Tinsert(ct_00, "warmup",	    HOFFSET(h5::bench::test_t,warmup), H5T_NATIVE_USHORT);
-        H5Tinsert(ct_00, "iteration",	HOFFSET(h5::bench::test_t,iteration), H5T_NATIVE_USHORT);
-        
-        H5Tinsert(ct_00, "min_runtime",	HOFFSET(h5::bench::test_t,min_runtime), H5T_NATIVE_DOUBLE);
-        H5Tinsert(ct_00, "avg_runtime",	HOFFSET(h5::bench::test_t,avg_runtime), H5T_NATIVE_DOUBLE);
-        H5Tinsert(ct_00, "max_runtime",	HOFFSET(h5::bench::test_t,max_runtime), H5T_NATIVE_DOUBLE);
-
-        H5Tinsert(ct_00, "min_throughput",	HOFFSET(h5::bench::test_t,min_throughput), H5T_NATIVE_DOUBLE);
-        H5Tinsert(ct_00, "avg_throughput",	HOFFSET(h5::bench::test_t,avg_throughput), H5T_NATIVE_DOUBLE);
-        H5Tinsert(ct_00, "max_throughput",	HOFFSET(h5::bench::test_t,max_throughput), H5T_NATIVE_DOUBLE);
-
-        H5Tinsert(ct_00, "name", HOFFSET(shim::pod_t,name), name_t);
-
-        H5Tclose(name_t); 
-        return ct_00;
-    };
-}
-H5CPP_REGISTER_STRUCT(h5::bench::test_t);
 
 namespace h5::bench::impl::tag {
     struct name_t {};
@@ -99,15 +64,27 @@ namespace h5::bench::impl {
         size_t value[N];
         size_t rank;
     };
+
+    template <class T>
+    std::pair<T,T> stats(const std::vector<T>& data){
+        T mean = std::accumulate(data.begin(), data.end(), 0) / (double)data.size();
+        T var  = std::accumulate(data.begin(), data.end(), 0, [&](const T&s, const T&v ) -> T {
+            T m = (v-mean);
+            return s + m*m;
+        }) / (double)data.size();
+        T std = sqrt(var);
+        return std::pair<T,T>{mean, std};
+    }
 }
 
 namespace h5::bench {
     constexpr size_t max_dims = 32;
     namespace arg = h5::meta::arg;
+    static void init(h5::fd_t fd, std::string report);
 
     using name = impl::value_t<std::string, impl::tag::name_t>;
-    using warmup = impl::value_t<unsigned, impl::tag::warmup_t>;
-    using sample = impl::value_t<unsigned, impl::tag::sample_t>;
+    using warmup = impl::value_t<uint16_t, impl::tag::warmup_t>;
+    using sample = impl::value_t<uint16_t, impl::tag::sample_t>;
     using arg_x = impl::array_t<impl::tag::x_t, max_dims>;
     using arg_y = impl::array_t<impl::tag::y_t ,max_dims>;
     using arg_z = impl::array_t<impl::tag::z_t, max_dims>;
@@ -121,19 +98,44 @@ namespace h5::bench {
             return instance;
         }
         
-        void push(const std::string& name, long double, long double, long double) {
-
+        void push(const test_t& data) {
+           this->list.push_back(data); 
         }
-
+        template<class V, class T>
+        void push(const std::string& name, uint64_t x, V warmup, V iteration, const std::pair<T,T>& time, const std::pair<T,T>& tp ) {
+            test_t item {
+                .warmup = warmup,
+                .iteration = iteration,
+                .x = x,
+                .mean_runtime = time.first,
+                .std_runtime = time.second,
+                .mean_throughput = tp.first,
+                .std_throughput = tp.second 
+            };
+            strncpy(item.name, name.data(), test_t::max_lenght::value);
+            list.push_back(item);
+        }
+        friend  void init(std::string, std::string);
         private:
-            store_t() {}
-            ~store_t(){
-                std::cout << "terminating ... \n";
+            store_t() : file_name("benchmark.h5"), dataset_name("/report"){
             }
-            std::vector<std::tuple<std::string, long double, long double, long double>> list;
+            ~store_t(){
+                fmt::print("[name                                    ][total events][Mi events/s] [ms runtime / stddev] [    MiB/s / stddev ]\n");
+
+                for(auto i: list)
+                    fmt::print("{:<42} {:>12} {:>12.4f}  {:>11.2f}  {:>8.3f} {:>10.2f}   {:>7.1f}\n",
+                    i.name, i.x, i.x / i.mean_runtime, i.mean_runtime/1000.0, i.std_runtime/1000.0, i.mean_throughput, i.std_throughput);
+            }
+            std::string file_name, dataset_name;
+            std::vector<test_t> list;
             unsigned warmup, iteration;
     };
 
+    void init(std::string file_name, std::string dataset_name){
+        store_t& store = store_t::get();
+        store.file_name = file_name;
+        store.dataset_name = dataset_name;
+    }
 
     template <class... args_t>
     static void throughput(args_t... args) {
@@ -147,20 +149,21 @@ namespace h5::bench {
         using warmup_t = typename arg::tpos<impl::tag::warmup_t, args_t...>;
         using sample_t = typename arg::tpos<impl::tag::sample_t, args_t...>;
         using callback_f = typename arg::tpos<callback_t, args_t...>;
-        
+        using pt_t = typename arg::tpos<pt_t, args_t...>;
+
         static_assert( name_t::present, "benchmark name must be specified..." );
 
         auto tuple = std::forward_as_tuple(args...);
         store_t& store = store_t::get();
         
-        warmup wu{3}; sample su{20};
+        warmup wu{3}; sample su{20}; name name = std::get<name_t::position>(tuple);
         arg_x x; arg_y y; arg_z z;
         if constexpr(x_t::present) x = std::get<x_t::position>(tuple);
         if constexpr(y_t::present) y = std::get<y_t::position>(tuple);
         if constexpr(z_t::present) z = std::get<z_t::position>(tuple);
         if constexpr(warmup_t::present) wu = std::get<warmup_t::position>(tuple);
         if constexpr(sample_t::present) su = std::get<sample_t::position>(tuple);
-        std::vector<long double> time, throughput;
+        std::vector<double> time, throughput;
 
         if constexpr( callback_f::present ){
             callback_t fn = std::get<callback_f::position>(tuple);
@@ -170,21 +173,28 @@ namespace h5::bench {
                 time.clear(); throughput.clear();
                 for(size_t i=0; i<su.value; i++) {
                     namespace cl = std::chrono;
+                    if constexpr (pt_t::present){
+                        h5::pt_t pt = std::get<pt_t::position>(tuple);
+                        h5::flush(pt);
+                    }
                     const auto start = cl::steady_clock::now();
-                        long double transfer_size = fn(j, x[j]);
+                        double transfer_size = fn(j, x[j]);
+                        if constexpr (pt_t::present){
+                            h5::pt_t pt = std::get<pt_t::position>(tuple);
+                            h5::flush(pt);
+                        }
                     const auto end = cl::steady_clock::now();
-                    const auto delta = static_cast<long double>(
-                        cl::duration_cast<cl::nanoseconds>(end - start).count());
+                    // time is in microsec, transfer_size in bytes -> thoughput in MiB/sec
+                    const auto delta = static_cast<double>(
+                        cl::duration_cast<cl::nanoseconds>(end - start).count()) / 1'000.0d;
                     time.push_back(delta);
                     // time is in nano seconds,  
-                    throughput.push_back(transfer_size /(delta /1'000.0)); 
+                    throughput.push_back(transfer_size /delta); 
                 }
-                auto avg_tp = std::accumulate(throughput.begin(), throughput.end(), 0) / throughput.size();
-                std::cout << " count: " << throughput.size() << " " << avg_tp   << " MiB/s\n";
+                auto tp = impl::stats(throughput);
+                auto rt = impl::stats(time);
+                store.push(name.value, x[j], wu.value, su.value, rt, tp);
             }
-
-
-            store.push("hello", 0.0, 0.0, 0.0);
         } 
     }
 
--- non-pod-struct-2021-apr-29/non-pod-struct-test.cpp
+++ non-pod-struct-2021-apr-29/non-pod-struct-test.cpp
@@ -1,34 +1,92 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <h5bench>
 #include <h5cpp/core>
-    #include "non-pod-struct.hpp"
+#include "non-pod-struct.hpp"
 #include <h5cpp/io>
-#include "h5bench.hpp"
+#include <fmt/core.h>
 
+namespace bh = h5::bench;
+bh::arg_x record_size{10'000, 100'000, 1'000'000};
+bh::warmup warmup{5};
+bh::sample sample{10};
+h5::chunk chunk{4096};
 
-namespace h3 = h5::bench;
-h3::arg_x record_size{100, 1000, 10'000, 100'000};
-
-int main(int argc, const char ** argv){
+int main(int argc, const char **argv)
+{
     size_t max_size = *std::max_element(record_size.begin(), record_size.end());
-    
+
     h5::fd_t fd = h5::create("h5cpp.h5", H5F_ACC_TRUNC);
-    h5::pt_t ds = h5::create<shim::pod_t>(fd, "with-id", h5::max_dims{H5S_UNLIMITED}, h5::chunk{4096}); 
     auto strings = h5::utils::get_test_data<std::string>(max_size);
-    std::vector<shim::pod_t> data(max_size);
-    for(size_t i=0; i<data.size(); i++)
-        data[i].id = i, strncpy(data[i].name, strings[i].data(), shim::pod_t::max_lenght::value);
-    // compute data transfer size:
-    std::vector<size_t> transfer_size;
-    for(auto i: record_size)
-        transfer_size.push_back(i*sizeof(shim::pod_t));
-    // actual measurement with burn in phase
-    h3::throughput(
-        h3::name{"bench_name"}, record_size, h3::warmup{3}, h3::sample{100}, 
-    [&](size_t idx, size_t size) -> long double {
-        for(size_t k=0; k<size; k++)
-            h5::append(ds, data[k]);
-        return transfer_size[idx];
-    });
+
+    // LETS PRINT PUT SOME STRINGS TO GIVE YOU THE PICTURE
+    fmt::print("[{:5>}] [{:^30}] [{:6}]\n", "#", "value", "lenght");
+    for(size_t i=0; i<10; i++) fmt::print("{:>2d}  {:>30}  {:>8d}\n", i, strings[i], strings[i].length());
+    fmt::print("\n\n");
+
+    { // POD: FIXED LENGTH STRING + ID
+        h5::pt_t ds = h5::create<shim::pod_t>(fd, "with-id", h5::max_dims{H5S_UNLIMITED}, chunk);
+        std::vector<shim::pod_t> data(max_size);
+        // we have to copy the string into the pos struct
+        for (size_t i = 0; i < data.size(); i++)
+            data[i].id = i, strncpy(data[i].name, strings[i].data(), shim::pod_t::max_lenght::value);
+
+        // compute data transfer size, we will be using this to measure throughput:
+        std::vector<size_t> transfer_size;
+        for (auto i : record_size)
+            transfer_size.push_back(i * sizeof(shim::pod_t));
+
+        // actual measurement with burn in phase
+        bh::throughput(
+            bh::name{"pod fixed length str"}, record_size, warmup, sample, ds,
+            [&](size_t idx, size_t size) -> double {
+                for (size_t k = 0; k < size; k++)
+                    h5::append(ds, data[k]);
+                return transfer_size[idx];
+            });
+    }
+
+    { // VL STRING, INDEXED BY HDF5 B+TREE
+        h5::pt_t ds = h5::create<std::string>(fd, "VL-string", h5::max_dims{H5S_UNLIMITED}, chunk);
+
+        // compute data transfer size:
+        std::vector<size_t> transfer_size;
+        for (size_t i =0, j=0, N = 0; i < strings.size(); i++){
+            N += strings[i].length();
+            if( i == record_size[j] - 1) j++, transfer_size.push_back(N);
+        }
+        // actual measurement with burn in phase
+        bh::throughput(
+            bh::name{"string with VL"}, record_size, warmup, sample,
+            [&](size_t idx, size_t size) -> double {
+                //std::cout << idx << " " << size << " " << strings.size() <<"\n";
+                for (size_t i = 0; i < size; i++)
+                    h5::append(ds, strings[i]);
+                return transfer_size[idx];
+            });
+    }
+    { // VL STRING, INDEXED BY HDF5 B+TREE std::vector<std::string>
+        std::vector<h5::ds_t> ds;
+        for(auto s:record_size) ds.push_back( h5::create<std::string>(fd, 
+            fmt::format("vector<std::string>-{:010d}", s), h5::current_dims{s}, chunk));
+        std::vector<char*> data(max_size);
+        // build a array of pointers to VL strings: one level of indirection 
+        for (size_t i = 0; i < data.size(); i++)
+            data[i] = strings[i].data();
+
+        // compute data transfer size:
+        std::vector<size_t> transfer_size;
+        for (size_t i =0, j=0, N = 0; i < strings.size(); i++){
+            N += strings[i].length();
+            if( i == record_size[j] - 1) j++, transfer_size.push_back(N);
+        }
+        // actual measurement with burn in phase
+        bh::throughput(
+            bh::name{"string with VL block write"}, record_size, warmup, sample,
+            [&](size_t idx, size_t size) -> double {
+                h5::write(ds[idx], data.data(), h5::count{size});
+                return transfer_size[idx];
+            });
+    }
 }
