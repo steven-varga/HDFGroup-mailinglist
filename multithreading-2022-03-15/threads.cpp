f45c906d5a9b598b78826b07e7041ee330c5b4ce
steven varga <steven@vargaconsulting.ca>
2022-03-16 22:59:34 -0400

    cleaned up implementation

--- multithreading-2022-03-15/threads.cpp
+++ multithreading-2022-03-15/threads.cpp
@@ -3,54 +3,39 @@
 #include <thread>
 #include "queue.hpp"
 #include "utils.hpp"
-#include <csignal>
-#include <iostream>
-
 
 constexpr int dataset_name_min=5;
 constexpr int dataset_name_max=21;
 constexpr int dataset_size=40000;
+constexpr int number_of_threads=60;
 
-using T = std::pair<std::string, std::vector<double>>;
-
-
-
-void signal_handler(int signal){
-	std::cout << "signal: " << signal <<std::endl;
-	exit(0);
-}
+struct task_t {
+	std::string name;
+	std::vector<double> data;
+};
 
 int main(){
-	using namespace std::chrono_literals;
-	
-	h5::exp::queue<T> io;
+	h5::exp::queue<task_t> io;
 
-	auto n_threads = 60;//std::jthread::hardware_concurrency();
+	auto n_threads = number_of_threads;
 	std::atomic<unsigned> n_jobs = n_threads;
 	std::vector<std::jthread> pool(n_threads); 
 
-	std::signal(SIGINT, signal_handler);
 	h5::fd_t fd = h5::create("example.h5", H5F_ACC_TRUNC);
 
 	std::jthread io_thread([&]{
 		while(n_jobs--){
-			auto data = io.pop();
-			{
-				h5::write(fd, data.first, data.second);
-			}
-			std::cout << data.first << "\n";
+			auto task = io.pop();
+			h5::write(fd, task.name, task.data);
 		}
-		std::cout << "done!!!\n";
-	
 	});
 
 	for(auto& current_thread: pool) 
 		current_thread = std::jthread([&]{
 			auto payload = h5::utils::get_test_data<double>(dataset_size);
 			std::string path = h5::utils::get_random_string(dataset_name_min, dataset_name_max);
-			// simulate workload:
-			std::this_thread::sleep_for(200ms);
-			io.push(std::make_pair(path, payload));
+			io.push(
+				task_t{path, payload});
 		});
 }
 
